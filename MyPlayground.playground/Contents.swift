368741ca8503e79b93fcef659492d3e3d9e993dc
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-09-22 16:47:10 +0900

    #04 基本的なデータ型を見ていこう

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var str = "Hello, playground"
+//Int
+//let i: Int = 10
+let i = 10
+
+//Float Double
+let d = 53.8
+
+//String
+let S = "hello"
+
+//Bool ture/false
+let flag = true
+
+var X = "five"
+X = "6"
+X = String(5)
