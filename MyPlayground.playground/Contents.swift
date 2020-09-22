f6eb5bd13a6ba6ca829c56a9710ec91a5eecb872
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-09-22 17:11:11 +0900

    #07 switchで条件分岐をしてみよう

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -26,3 +26,24 @@ let flag = true
 var X = "five"
 X = "6"
 X = String(5)
+
+//NO.7
+//switch
+
+let num = 10
+
+switch num {
+case 0:
+    print ("zero")
+case 1,2,3:
+    print("small")
+case 4...6:
+    print ("4/5/6")
+case 7..<9:
+    print("7/8")
+case let n where n > 20:
+    print("\(n) is huge!")
+default:
+//    print("n.a")
+    break
+}
