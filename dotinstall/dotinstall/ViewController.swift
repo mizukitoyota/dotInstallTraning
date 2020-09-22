220d281248cb498d0c2c4b96b6b6f7fadb504af3
MizukiToyota <mtoyota@system-renovate.co.jp>
2020-09-22 15:32:49 +0900

    初期設定

---
+++ dotinstall/dotinstall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dotinstall
+//
+//  Created by Training on 2020/09/22.
+//  Copyright © 2020 training. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
